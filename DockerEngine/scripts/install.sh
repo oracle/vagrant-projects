db4f2e39593d64cfa7642132aea5247c1e295f59
Sergio Leunissen <sergio.leunissen@oracle.com>
2018-02-28 12:06:34 -0500

    Adding DockerEngine Vagrantfile
    This Vagrantfile and provisioning script sets up Docker engine on the
    latest Oracle Linux 7 box with Btrfs as storage for images

---
+++ DockerEngine/scripts/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# LICENSE UPL 1.0
+#
+# Copyright (c) 1982-2018 Oracle and/or its affiliates. All rights reserved.
+# 
+# Since: February, 2018
+# Author: sergio.leunissen@oracle.com
+# Description: Installs Docker engine using Btrfs as storage
+# 
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+#
+echo 'Installing and configuring Docker engine'
+
+# install Docker engine
+yum -y install docker-engine
+
+# Format spare device as Btrfs
+# Configure Btrfs storage driver
+
+docker-storage-config -s btrfs -d /dev/sdb
+
+# Start and enable Docker engine
+systemctl start docker
+systemctl enable docker
+
+echo 'Docker engine is ready to use'
+echo 'To get started, on your host, run:'
+echo '  vagrant ssh'
+echo 
+echo 'Then, within the guest (for example):'
+echo '  sudo docker run -it oraclelinux:6-slim'
+echo

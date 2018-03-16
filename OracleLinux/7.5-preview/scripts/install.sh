1021525e7ad6811363d85ba08dca7b8f4098a8da
Simon Coter <simon.coter@gmail.com>
2018-03-16 14:03:28 +0100

    Fixed Readme with 'vagrant status' command

--- OracleLinux/7.5-preview/scripts/install.sh
+++ OracleLinux/7.5-preview/scripts/install.sh
@@ -36,18 +36,26 @@ echo 'gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-oracle' >> /etc/yum.repos.d/pub
 echo 'gpgcheck=1' >> /etc/yum.repos.d/public-yum-ol7.repo
 echo 'enabled=1' >> /etc/yum.repos.d/public-yum-ol7.repo
 
-echo 'Optional - Add Proxy to Yum configuration'
-#echo 'proxy=http://<Proxy-Server-IP-Address>:<Proxy_Port>' >> /etc/yum.conf
-#echo 'proxy_username=<Proxy-User-Name>' >> /etc/yum.conf
-#echo 'proxy_password=<Proxy-Password>' >> /etc/yum.conf
-
 # get up to date
 echo 'Getting OL 7.5 Preview Updates.....'
 yum upgrade -y
 
+# update motd for Oracle Linux 7.5 Preview
+echo '' > /etc/motd
+echo 'Welcome to Oracle Linux Server release 7.5 Preview' >> /etc/motd
+echo '' >> /etc/motd
+echo 'The Oracle Linux End-User License Agreement can be viewed here:' >> /etc/motd
+echo '' >> /etc/motd
+echo '    * /usr/share/eula/eula.en_US' >> /etc/motd
+echo '' >> /etc/motd
+echo 'For additional packages, updates, documentation and community help, see:' >> /etc/motd
+echo '' >> /etc/motd
+echo '    * http://yum.oracle.com/' >> /etc/motd
+echo '' >> /etc/motd
+
 # install kmod rpms guest-add
-echo 'Installing VirtualBox Guest Additions - RPMs for Oracle Linux'
-#yum install kmod-vboxguest-uek5-5.2.8-1.el7.x86_64 vboxguest-tools-5.2.8-1.el7.x86_64 -y
+# echo 'Installing VirtualBox Guest Additions - RPMs for Oracle Linux'
+# yum install kmod-vboxguest-uek5-5.2.8-1.el7.x86_64 vboxguest-tools-5.2.8-1.el7.x86_64 -y
 
 # fix locale warning
 echo LANG=en_US.utf-8 >> /etc/environment
@@ -56,7 +64,26 @@ echo LC_ALL=en_US.utf-8 >> /etc/environment
 echo 'INSTALLER: Locale set'
 
 echo 'INSTALLER: System updated'
-echo 'INSTALLER: Going to reboot to get updated system'
-reboot
 
-echo 'INSTALLER: Installation complete, Oracle Linux ready to use!'
+echo 'INSTALLER: Upgrading VirtualBox Guest Additions to 5.2 for UEK5'
+echo ''
+echo 'INSTALLER: Getting required dependencies...'
+yum install bzip2 kernel-uek-devel -y
+wget https://download.virtualbox.org/virtualbox/5.2.8/VBoxGuestAdditions_5.2.8.iso -O /tmp/VBoxGuestAdditions_5.2.8.iso -o /tmp/vboxguestadd-download.log
+
+echo 'INSTALLER: Installing VirtualBox Guest Additions 5.2.8...'
+mount -o loop /tmp/VBoxGuestAdditions_5.2.8.iso /media
+/media/VBoxLinuxAdditions.run
+
+echo 'INSTALLER: Compiling VirtualBox Modules for UEK5 Kernel...'
+echo 'export KERN_VER=`ls /lib/modules |grep 4.14`' > /tmp/compile_vboxga_uek5.sh
+echo '/sbin/rcvboxadd setup' >> /tmp/compile_vboxga_uek5.sh
+chmod 700 /tmp/compile_vboxga_uek5.sh
+/tmp/compile_vboxga_uek5.sh
+rm -f /tmp/compile_vboxga_uek5.sh
+
+echo 'INSTALLER: Cleaning up installation...'
+umount /media
+rm -f /tmp/VBoxGuestAdditions_5.2.8.iso
+
+echo 'INSTALLER: Going to reboot to get updated system with UEK5'

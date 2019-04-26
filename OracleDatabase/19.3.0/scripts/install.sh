58aebe363323ee627669e8e3d164652510316d38
Gerald Venzl <gerald.venzl@oracle.com>
2019-04-26 10:03:51 -0700

    Rename 19c binaries file
    Signed-off-by: Gerald Venzl <gerald.venzl@oracle.com>

--- OracleDatabase/19.3.0/scripts/install.sh
+++ OracleDatabase/19.3.0/scripts/install.sh
@@ -51,7 +51,7 @@ echo 'INSTALLER: Environment variables set'
 
 # Install Oracle
 
-unzip /vagrant/db_home.19.3.zip -d $ORACLE_HOME/
+unzip /vagrant/LINUX.X64_193000_db_home.zip -d $ORACLE_HOME/
 cp /vagrant/ora-response/db_install.rsp.tmpl /vagrant/ora-response/db_install.rsp
 sed -i -e "s|###ORACLE_BASE###|$ORACLE_BASE|g" /vagrant/ora-response/db_install.rsp && \
 sed -i -e "s|###ORACLE_HOME###|$ORACLE_HOME|g" /vagrant/ora-response/db_install.rsp && \

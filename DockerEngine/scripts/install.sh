0a3622d87e51081e92f423f5afbb4be026cb5fde
Philippe Vanhaesendonck <philippe.vanhaesendonck@e-bulles.be>
2020-05-12 15:32:01 +0200

    Add support for libvirt provider
    Signed-off-by: Philippe Vanhaesendonck <philippe.vanhaesendonck@e-bulles.be>

--- DockerEngine/scripts/install.sh
+++ DockerEngine/scripts/install.sh
@@ -2,12 +2,12 @@
 #
 # LICENSE UPL 1.0
 #
-# Copyright (c) 1982-2018 Oracle and/or its affiliates. All rights reserved.
-# 
+# Copyright (c) 2018, 2020 Oracle and/or its affiliates.
+#
 # Since: February, 2018
 # Author: sergio.leunissen@oracle.com
 # Description: Installs Docker engine using Btrfs as storage
-# 
+#
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 #
 echo 'Installing and configuring Docker engine'
@@ -18,7 +18,7 @@ yum -y install docker-engine
 # Format spare device as Btrfs
 # Configure Btrfs storage driver
 
-docker-storage-config -s btrfs -d /dev/sdb
+docker-storage-config -s btrfs -d /dev/[sv]db
 
 # Start and enable Docker engine
 systemctl start docker
@@ -33,7 +33,7 @@ chmod a+x /etc/docker
 echo 'Docker engine is ready to use'
 echo 'To get started, on your host, run:'
 echo '  vagrant ssh'
-echo 
+echo
 echo 'Then, within the guest (for example):'
 echo '  docker run -it oraclelinux:6-slim'
 echo

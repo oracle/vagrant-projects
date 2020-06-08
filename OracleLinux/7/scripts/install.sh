a247528e5905bc8e7a16035cf8c80071664fa75c
Philippe Vanhaesendonck <philippe.vanhaesendonck@oracle.com>
2020-06-08 15:16:58 +0200

    Don't install plugins in Vagrantfiles
    Signed-off-by: Philippe Vanhaesendonck
    <philippe.vanhaesendonck@oracle.com>

--- OracleLinux/7/scripts/install.sh
+++ OracleLinux/7/scripts/install.sh
@@ -2,12 +2,12 @@
 #
 # LICENSE UPL 1.0
 #
-# Copyright (c) 1982-2018 Oracle and/or its affiliates. All rights reserved.
-# 
+# Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+#
 # Since: January, 2018
 # Author: gerald.venzl@oracle.com
 # Description: Updates Oracle Linux to the latest version
-# 
+#
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 #
 
@@ -23,5 +23,3 @@ echo LANG=en_US.utf-8 >> /etc/environment
 echo LC_ALL=en_US.utf-8 >> /etc/environment
 
 echo 'INSTALLER: Locale set'
-
-echo 'INSTALLER: Going to reboot to get updated system'
